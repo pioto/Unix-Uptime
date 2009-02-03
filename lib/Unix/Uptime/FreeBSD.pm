38cda0debf1089447d32090a852c031a648a1bcb
Mike Kelly <pioto@pioto.org>
2009-02-03 12:17:07 -0500

    Use an absolute path to sysctl.
    Some people don't have /sbin in their path...

--- lib/Unix/Uptime/FreeBSD.pm
+++ lib/Unix/Uptime/FreeBSD.pm
@@ -9,7 +9,7 @@ $VERSION = eval $VERSION;
 sub uptime {
     my $class = shift;
     
-    my $boottime = `sysctl kern.boottime`;
+    my $boottime = `/sbin/sysctl kern.boottime`;
     my ($boot_seconds,$boot_useconds) = $boottime =~ /\s+sec\s+=\s+(\d+),\s+usec\s+=\s+(\d+)/;
     return time() - $boot_seconds;
 }
@@ -21,7 +21,7 @@ sub uptime_hires {
         die "uptime_hires: you need to import Unix::Uptime with ':hires'";
     }
 
-    my $boottime = `sysctl kern.boottime`;
+    my $boottime = `/sbin/sysctl kern.boottime`;
     my ($boot_seconds,$boot_useconds) = $boottime =~ /\s+sec\s+=\s+(\d+),\s+usec\s+=\s+(\d+)/;
     my $time = Time::HiRes::gettimeofday();
     my $boot_time = $boot_seconds + ($boot_useconds * (10.0**-6));
