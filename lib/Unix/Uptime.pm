f7d78790b2ff0f4a4943eecfe3a5623686036f38
Mike Kelly <pioto@pioto.org>
2009-09-03 10:12:27 -0400

    Update licensing information for better style.
    Using recommendations from
    <http://www.perlfoundation.org/cpan_licensing_guidelines>.

--- lib/Unix/Uptime.pm
+++ lib/Unix/Uptime.pm
@@ -157,10 +157,16 @@ Mike Kelly <pioto@pioto.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008, 2009 Mike Kelly
+Copyright (C) 2008-2009, Mike Kelly.
 
-Distributed under the same terms as Perl itself. See
-L<http://dev.perl.org/licenses/> for more information.
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl 5.10.0. For more details, see the full text
+of the licenses at <http://www.perlfoundation.org/artistic_license_1_0>,
+and <http://www.gnu.org/licenses/gpl-2.0.html>.
+
+This program is distributed in the hope that it will be
+useful, but without any warranty; without even the implied
+warranty of merchantability or fitness for a particular purpose.
 
 =cut
 
