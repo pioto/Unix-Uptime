8a4cf5d2b4964ce2ea66bb64cd79adcac5363093
Mike Kelly <pioto@pioto.org>
2010-12-17 09:03:50 -0500

    update copyright date, just about in time for 2011...

--- lib/Unix/Uptime.pm
+++ lib/Unix/Uptime.pm
@@ -155,7 +155,7 @@ Mike Kelly <pioto@pioto.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2008-2009, Mike Kelly.
+Copyright (C) 2008-2010, Mike Kelly.
 
 This module is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
